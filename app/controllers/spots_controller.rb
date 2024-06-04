ba67a5e1f13d850b254c6e3546af740394887813
Manon Reynaud <manon.reynaud2@gmail.com>
2024-06-04 15:22:13 +0200

    Setup mapbox carte

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,5 +1,13 @@
 class SpotsController < ApplicationController
   def index
+    @spots = Spot.all
+    @markers = @spots.geocoded.map do |spot|
+      {
+        lat: spot.latitude,
+        lng: spot.longitude,
+        marker_html: render_to_string(partial: "marker")
+      }
+    end
   end
 
   def show
