41190d9bc73268a6b031467278251d51c5a81a33
Thomas Mascheretti <thomasmascheretti@yahoo.fr>
2024-06-04 12:14:28 +0200

    [TAS-62]Setup Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create([
+  { email: 'jean.dolet@example.com', password: 'password123', first_name: 'Jean', last_name: 'Dolet', birthdate: '1990-01-01', level: 1 },
+  { email: 'jeanne.smet@example.com', password: 'password123', first_name: 'Jeanne', last_name: 'Smet', birthdate: '1985-02-14', level: 2 },
+  { email: 'alice.saunier@example.com', password: 'password123', first_name: 'Alice', last_name: 'Saunier', birthdate: '1992-03-22', level: 3 },
+  { email: 'bernard.broule@example.com', password: 'password123', first_name: 'Bernard', last_name: 'Broule', birthdate: '1988-04-10', level: 1 },
+  { email: 'charlie.moret@example.com', password: 'password123', first_name: 'Charlie', last_name: 'Moret', birthdate: '1993-05-30', level: 2 },
+  { email: 'diana.gouetta@example.com', password: 'password123', first_name: 'Diana', last_name: 'Gouetta', birthdate: '1991-06-25', level: 3 },
+  { email: 'hugo.hersant@example.com', password: 'password123', first_name: 'Hugo', last_name: 'Hersant', birthdate: '1987-07-15', level: 1 },
+  { email: 'fiona.sala@example.com', password: 'password123', first_name: 'Fiona', last_name: 'Sala', birthdate: '1989-08-20', level: 2 },
+  { email: 'george.claron@example.com', password: 'password123', first_name: 'George', last_name: 'Claron', birthdate: '1994-09-12', level: 3 },
+  { email: 'anna.retali@example.com', password: 'password123', first_name: 'Anna', last_name: 'Retali', birthdate: '1986-10-05', level: 1 }
+])
+
