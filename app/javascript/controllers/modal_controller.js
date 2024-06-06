8eaf664d9fa18e03a6562414771ffe5b99d649b5
Louis de Place <louis@de-place.fr>
2024-06-06 16:02:05 +0200

    Setup pwa, changed the homepage modal video and did some minor refacto

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,23 +6,48 @@ export default class extends Controller {
   connect() {
     this.element.classList.add("show");
     this.element.classList.add("d-block");
+    this.isScrolling = false;
+    this.scrollTimeout = null;
   }
 
-  close() {
+  handleScroll() {
+    // Set isScrolling to true on scroll
+    this.isScrolling = true;
+  }
+
+  handleTouchEnd() {
+    // User has removed their finger from the screen
+    this.checkScrollEnd();
+  }
+
+  checkScrollEnd() {
+    // Clear any previous timeout
+    clearTimeout(this.scrollTimeout);
+
+    // Set a timeout to check if the scroll has stopped after a short delay
+    this.scrollTimeout = setTimeout(() => {
+      if (this.isScrolling) {
+        this.isScrolling = false;
+        this.onScrollEnd();
+      }
+    }, 100); // Adjust the delay as needed
+  }
+
+  onScrollEnd() {
     const minDistance = 400;
     const container = this.element;
+
     if (container.scrollTop > minDistance) {
-      this.emptyTarget.scrollIntoView({
-        behavior: "smooth",
-      });
-      if (container.scrollTop === container.clientHeight) {
+      this.emptyTarget.scrollIntoView({ behavior: "smooth" });
+      if (
+        container.scrollTop + container.clientHeight >=
+        container.scrollHeight
+      ) {
         this.element.classList.remove("show");
         this.element.classList.remove("d-block");
       }
     } else {
-      this.videoTarget.scrollIntoView({
-        behavior: "smooth",
-      });
+      this.videoTarget.scrollIntoView({ behavior: "smooth" });
     }
   }
 }
