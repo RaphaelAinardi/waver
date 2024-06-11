43341a25caa990f51eabb910e58c14e4e260e41b
Manon Reynaud <manon.reynaud2@gmail.com>
2024-06-11 11:47:49 +0200

    [TAS-31]logout_update_avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
   end
+
+  def user_root_path
+    dashboard_path
+  end
+
+  def after_sign_in_path(resources)
+    spots_path
+  end
+
+  def after_sign_out_path_for(resources)
+    spots_path
+  end
 end
