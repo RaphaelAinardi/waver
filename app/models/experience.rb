b56b0b4ebfda005033bd668a60eb3c8e25d59497
Raphael Ainardi <ainardi.raphael@icloud.com>
2024-06-13 20:56:54 +0200

    fix deploy

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,6 +2,7 @@ class Experience < ApplicationRecord
   belongs_to :spot
   belongs_to :user
   has_many :comments
+  has_many :likes, dependent: :destroy
 
   validates :content, presence: true
   has_many_attached :photos
