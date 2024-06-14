d11fe56b8a0e83a26477f89c1869dfce0e17489d
Manon Reynaud <manon.reynaud2@gmail.com>
2024-06-14 11:45:51 +0200

    improve seeds

--- db/schema.rb
+++ db/schema.rb
@@ -156,7 +156,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_13_225028) do
     t.string "last_name"
     t.date "birthdate"
     t.integer "level"
-    t.string "boards"
+    t.string "board"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
